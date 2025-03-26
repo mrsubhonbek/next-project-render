21208bcd1f24609e192bc8457ef7498b8eab0128
asadulloev_subhonbek <asadulloev_subhonbek@enot.work>
2025-03-26 18:11:28 +0300

    chor: add layout route

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,18 +9,14 @@ export const metadata: Metadata = {
     default: 'Next.js App Router',
     template: '%s | Next.js App Router',
   },
-  metadataBase: new URL('https://app-router.vercel.app'),
   description:
     'A playground to explore new Next.js App Router features such as nested layouts, instant loading states, streaming, and component level data fetching.',
   openGraph: {
-    title: 'Next.js App Router Playground',
+    title: 'Next.js App Router',
     description:
       'A playground to explore new Next.js App Router features such as nested layouts, instant loading states, streaming, and component level data fetching.',
-    images: [`/api/og?title=Next.js App Router`],
   },
-  twitter: {
-    card: 'summary_large_image',
-  },
+
 };
 
 export default function RootLayout({
