2260f4c925267a5185f890e1a2fbda9cb8802956
asadulloev_subhonbek <asadulloev_subhonbek@enot.work>
2025-03-27 14:52:30 +0300

    feat: styling with next js

---
+++ app/styling/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div className="prose prose-sm prose-invert max-w-none">
+      <h1 className="text-xl font-bold">Styling</h1>
+
+      <ul>
+        <li>This example shows different styling solutions.</li>
+      </ul>
+    </div>
+  );
+}
