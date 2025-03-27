856c28153493a5f75e05dc366cb1b36346158f22
asadulloev_subhonbek <asadulloev_subhonbek@enot.work>
2025-03-27 11:16:51 +0300

    feat: add not-found page

---
+++ app/not-found/[categorySlug]/layout.tsx
@@ -0,0 +1,46 @@
+import { getCategories, getCategory } from '#/app/api/categories/getCategories';
+import { ClickCounter } from '#/ui/click-counter';
+import { TabGroup } from '#/ui/tab-group';
+
+export default async function Layout(props: {
+  children: React.ReactNode;
+  params: Promise<{ categorySlug: string }>;
+}) {
+  const params = await props.params;
+
+  const { children } = props;
+
+  const category = await getCategory({ slug: params.categorySlug });
+  const categories = await getCategories({ parent: params.categorySlug });
+
+  return (
+    <div className="space-y-9">
+      <div>
+        <div className="flex justify-between">
+          <TabGroup
+            path={`/not-found/${category.slug}`}
+            items={[
+              {
+                text: 'All',
+              },
+              ...categories.map((x) => ({
+                text: x.name,
+                slug: x.slug,
+              })),
+              {
+                text: 'Subcategory That Does Not Exist',
+                slug: 'does-not-exist',
+              },
+            ]}
+          />
+
+          <div className="self-start">
+            <ClickCounter />
+          </div>
+        </div>
+      </div>
+
+      <div>{children}</div>
+    </div>
+  );
+}
